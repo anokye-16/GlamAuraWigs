cd3109a70f1cd86b90e69371d072d49c4fcd6ade
GlamAura User <user@example.com>
2026-04-30 22:06:00 +0000

    Disable foreign key checks in import script

--- import_db.php
+++ import_db.php
@@ -22,6 +22,9 @@ $sql = file_get_contents($sql_file);
 $sql = preg_replace('/--.*$/m', '', $sql);
 $sql = preg_replace('/\/\*.*?\*\//s', '', $sql);
 
+// Disable foreign key checks to allow truncate/drop
+$conn->query("SET FOREIGN_KEY_CHECKS = 0;");
+
 if ($conn->multi_query($sql)) {
     do {
         /* store first result set */
@@ -29,6 +32,10 @@ if ($conn->multi_query($sql)) {
             $result->free();
         }
     } while ($conn->next_result());
+    
+    // Re-enable foreign key checks
+    $conn->query("SET FOREIGN_KEY_CHECKS = 1;");
+    
     echo "<p style='color:green; font-size:24px;'>✅ SUCCESS! Your database has been imported. All tables and products are now live!</p>";
     echo "<p><a href='index.php'>Go to Homepage</a></p>";
 } else {
