50cb240e65eb9fad5826f73b4edce8383d2f754c
GlamAura User <user@example.com>
2026-04-30 20:02:56 +0000

    Fix case sensitivity for home.html

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
-echo "<h1>GlamAura Wigs Live Test</h1>";
-echo "<p>If you can see this, the PHP server is working!</p>";
-echo "<hr>";
-include 'home.html';
+// Try to find the home page file (checking for case sensitivity)
+$files = ['home.html', 'Home.html', 'HOME.HTML'];
+$found = false;
+
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        include $file;
+        $found = true;
+        break;
+    }
+}
+
+if (!$found) {
+    echo "<h1>GlamAura Wigs</h1>";
+    echo "<p>Error: Could not find the home page file. Please check your filenames.</p>";
+    // List files to help debug
+    echo "<h3>Files found in directory:</h3><ul>";
+    foreach (scandir('.') as $f) {
+        if ($f !== '.' && $f !== '..') echo "<li>$f</li>";
+    }
+    echo "</ul>";
+}
 ?>
